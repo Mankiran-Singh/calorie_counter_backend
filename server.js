4bf2c701aa61dc004f42cc5a11cbf295fc89d315
mankiran8910 <94637111+mankiran8910@users.noreply.github.com>
2024-12-27 12:40:41 +0530

    atlas setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config()
+const dotenv=require('dotenv');
+
+dotenv.config({path:'./config.env'});
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const entryRoutes = require('./routes/entryRoutes');
@@ -9,10 +11,21 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+// console.log(process.env.MONGO_URI)
+const mongo_uri=process.env.MONGO_URI
+mongoose.connect(mongo_uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB Atlas');
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
 });
+
+const Entry = require('./models/Entry');
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
@@ -22,7 +35,7 @@ db.once('open', () => {
 
 app.use('/api/entries', entryRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port=process.env.PORT
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
